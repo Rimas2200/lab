e95dc91915884e56de5de2fe6d7f0fbe435591a1
Rimas <dmitrij.karpenko.83@inbox.ru>
2024-04-08 08:15:11 +0500

    lab

---
+++ lab1.py
@@ -0,0 +1,38 @@
+import random
+import math
+import matplotlib.pyplot as plt
+
+t0 = (1, 0)
+t1 = (1.707, 0.707)
+t2 = (1, 1)
+t3 = (0.293, 0.707)
+t4 = (0, 0)
+t5 = (0.293, -0.707)
+t6 = (1, -1)
+t7 = (1.707, -0.707)
+
+points = []
+
+p0 = random.choice([t0, t1, t2])
+points.append(p0)
+points.append(t3)
+points.append(t4)
+points.append(t5)
+points.append(t6)
+points.append(t7)
+
+for _ in range(50000):
+    # случайная точка ti из t0, t1, t2
+    ti = random.choice([t0, t1, t2, t3, t4, t5, t6, t7])
+    # середина отрезка p0ti
+    p1 = ((p0[0] + ti[0]) / 2, (p0[1] + ti[1]) / 2)
+    points.append(p1)
+    p0 = p1
+
+x_values, y_values = zip(*points)
+
+plt.scatter(x_values, y_values, s=1)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('lab1')
+plt.show()
